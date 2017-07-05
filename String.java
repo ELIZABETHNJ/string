89886fe351337d8e8fb9ed6c18227d11b1bc9c74
ELIZABETHNJ <elizabethnj1996@gmail.com>
2017-07-05 14:34:34 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,14 @@
+# string
+import java.io.*;
+import java.util.*;
+public class String
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner (System.in);
+    System.out.println("Enter the integer");
+    String x=sc.nextLine();
+    int a=Integer.parseInt(x);
+    System.out.println(a);
+  }
+}
